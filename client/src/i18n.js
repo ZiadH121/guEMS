1f182c5275e28a4dc6c88a054375d3570c6a4e50
ZiadH121 <ziadd.husseinn@gmail.com>
2025-10-29 19:46:07 +0300

    chore(translation - front): simplify translation directories for ease of access

--- client/src/i18n.js
+++ client/src/i18n.js
@@ -2,8 +2,8 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-import translationEN from './locales/en/translation.json';
-import translationAR from './locales/ar/translation.json';
+import translationEN from './locales/en.json';
+import translationAR from './locales/ar.json';
 
 i18n
   .use(LanguageDetector)
